48db923fc3192ed3aa8cfd58a52a722501e2dc49
HamiltonMas <149901203+HamiltonMas@users.noreply.github.com>
2024-09-13 11:05:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from "react";
 import "./App.css";
 import Table from "./components/Table";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <div className="header-left">
-          <i className="fa-regular fa-clock" aria-hidden="true"></i>
-          <div className="header-text">
-            <h2>ALL ISSUES</h2>
-            <p>See all issues you've been assigned to and worked on</p>
-          </div>
-        </div>
-        <div className="header-right">
-          <input type="text" className="search-input" placeholder="search" />
-          <i className="fa fa-search search-icon" aria-hidden="true"></i>
-          <div className="filter-sort-container">
-            <button className="filter-button">
-              <i className="fa-solid fa-filter" aria-hidden="true"></i>
-              <p> Filter</p>
-            </button>
-            <button className="sort-button">
-              <i className="fa-solid fa-list" aria-hidden="true"></i> Sort
-            </button>
-          </div>
-        </div>
-      </header>
+      <Header />
       <Table />
     </div>
   );
